02fa30e4d73fab22cfc71dc65f2ca667427d5ef5
Tim Rybicki <t.rybicki@gmail.com>
2019-07-18 17:01:53 -0400

    Add conditional to stop page error

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import { scrapedData } from './scrapedData'
 
 function App() {
   const mappedItems = Object.keys(scrapedData.map(item => {
-    const episodes = Object.values(item);
-    console.log(episodes)
 
-    episodes[0][0] && console.log('double', episodes[0].map(test => {
-      console.log(test.textContent)
-    }))
+    if (Object.entries(item).length !== 0) {
+      const episodes = Object.values(item);
+      // console.log(episodes)
+       return episodes[0].map(entry => {
+        console.log('entry', entry.textContent)
+        console.log('hyperlink', entry.hyperlink)
+        return <div>
+          {entry.textContent}
+        </div>
+      })
+    }
+
 
   }))
   return (
