68da6a62412916065143ead7ffd643e83caf994d
trybick <t.rybicki@gmail.com>
2019-07-18 20:21:40 -0400

    Install eslint and prettier

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import { scrapedData } from './scrapedData'
+import { scrapedData } from './scrapedData';
 
 function App() {
-  // Remove empty objects
   const filteredData = scrapedData.filter(item => Object.entries(item).length !== 0);
 
-  const mappedItems = Object.keys(filteredData.map(item => {
+  const mappedItems = Object.keys(
+    filteredData.map(item => {
       const episodes = Object.values(item);
-      
+
       const step2 = episodes[0].map(x => {
-        console.log('entry', x.textContent)
-        console.log('hyperlink', x.hyperlink)
-        return <div>
-          {x.textContent}
-        </div>
-      })
-
-
-  }))
+        console.log('text', x.textContent);
+        console.log('hyperlink', x.hyperlink);
+        return <div>{x.textContent}</div>;
+      });
+    }),
+  );
   return (
     <div>
       'test'
