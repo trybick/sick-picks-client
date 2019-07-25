4bf25be459ef6f54eefd266eb8d037de4bb612ff
trybick <t.rybicki@gmail.com>
2019-07-25 11:51:24 -0400

    Add media queries for screen sizes for app container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-// import { Container } from 'semantic-ui-react';
 import { Header } from './components/common';
 import { SickPicks } from './components/SickPicks';
 import './App.scss'
 
 function App() {
   return (
-    <div className="app">
+    <div className="container">
       <Header />
       <SickPicks />
     </div>
