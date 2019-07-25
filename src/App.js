07288646786c7aedfb5e734d8bad9fddaec9efbd
trybick <t.rybicki@gmail.com>
2019-07-25 11:41:16 -0400

    Make app class with max width

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Container } from 'semantic-ui-react';
+// import { Container } from 'semantic-ui-react';
 import { Header } from './components/common';
 import { SickPicks } from './components/SickPicks';
+import './App.scss'
 
 function App() {
   return (
-    <Container>
+    <div className="app">
       <Header />
       <SickPicks />
-    </Container>
+    </div>
   );
 }
 
