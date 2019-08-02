e04ad6079b493447831253fd172a47491db52043
trybick <t.rybicki@gmail.com>
2019-08-01 20:23:25 -0400

    Add github corner

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import React from 'react';
 import { Header } from './components/common';
 import { SickPicks } from './components/SickPicks';
 import './App.scss';
+import GithubCorner from 'react-github-corner';
+
+const githubCorner = (
+  <GithubCorner
+    href="https://github.com/trybick/sick-picks-client"
+    bannerColor="#151513"
+    octoColor="#fff"
+    size={80}
+    direction="right"
+    ariaLabel="Open GitHub project"
+  />
+);
 
 function App() {
   return (
     <div className="container">
+      {githubCorner}
       <Header />
       <SickPicks />
     </div>
